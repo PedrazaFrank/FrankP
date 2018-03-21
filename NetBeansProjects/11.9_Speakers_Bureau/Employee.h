81b9259a6e0647e2f4cbd317066a3a7dc3e6a0b2
Frank Pedraza <pedraza.frankie@gmail.com>
2018-03-21 01:53:42 -0700

    Signed-off-by: Frank Pedraza <pedraza.frankie@gmail.com>

---
+++ NetBeansProjects/11.9_Speakers_Bureau/Employee.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Employee.h
+ * Author: Frank Pedraza
+ *
+ * Created on March 21, 2018
+ */
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+//Structure Declaration
+struct employee{
+    string name;
+    string phoneNumber;
+    string topic;
+    int fee;  
+};
+
+#endif /* EMPLOYEE_H */
+
--- NetBeansProjects/11.9_Speakers_Bureau/main.cpp
+++ NetBeansProjects/11.9_Speakers_Bureau/main.cpp
@@ -9,24 +9,28 @@
 //System Libraries Here
 #include <cstdlib>
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 //User Libraries Here
 
 //Global Constants Only, No Global Variables
 //Like PI, e, Gravity, or conversions
-const int NUM_SPEAKERS=10;
+const int NUM_SPEAKERS=2;
 
-//Function Prototypes Here
-
+//Declare Structure
 struct employee{
     string name;
     string phoneNumber;
     string topic;
     int fee;
-    
 };
+
+//Function Prototypes Here
+void FillStruct(employee *);
+void EditStruct(employee *);
+void PrintStruct(employee *);
+
 //Program Execution Begins Here
 int main(int argc, char** argv) {
     //Declare array for structures for speakers
@@ -45,13 +49,14 @@ int main(int argc, char** argv) {
         cin >> choice[0];
         
         if(choice[0]=='1'){
-            cout << "Option 1" << endl;
+            FillStruct(speakers);
         }
         else if(choice[0]=='2'){
             cout << "Option 2" << endl;
+            //EditStruct(speakers);
         }
         else if(choice[0]=='3'){
-            cout << "Option 3" << endl;
+            PrintStruct(speakers);
         }
         else if(choice[0]=='4'){
             return 0;
@@ -67,3 +72,32 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+void FillStruct(employee *s){
+    
+    for (int i=0; i<NUM_SPEAKERS; i++){
+        cout << "Enter the name of the speaker: ";
+        cin.ignore();
+        getline(cin, s[i].name);
+        cout << "Enter the phone number of the speaker: ";
+        cin.ignore();
+        getline(cin,s[i].phoneNumber);
+        cout << "Enter the speaker's topic: ";
+        cin.ignore();
+        getline(cin,s[i].topic);
+        cout << "Enter the speaker's fee: ";
+        cin.ignore();
+        cin >> s[i].fee;
+        cout << endl;
+    }
+    
+}
+
+void PrintSctruct(employee *s){
+    for(int i=0; i<NUM_SPEAKERS; i++){
+        cout << "Name: " << s[i].name << endl;
+        cout << "Phone Number: " << s[i].phoneNumber << endl;
+        cout << "Topic: " << s[i].topic << endl;
+        cout << "Fee: " << s[i].fee << endl;
+        cout << endl;
+    }
+}
